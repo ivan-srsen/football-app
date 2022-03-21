17d55e96205e425d5aa403313ad4ab50b968da57
unknown <i.srsen9@gmail.com>
2022-03-21 13:08:45 +0100

    Test

--- Server/src/Football.Api/Features/Games/GameCreate.cs
+++ Server/src/Football.Api/Features/Games/GameCreate.cs
@@ -8,8 +8,6 @@ namespace Football.Api.Features.Games
 {
     public class GameCreateCommand : IRequest
     {
-        public string Team1Name { get; set; }
-        public string Team2Name { get; set; }
         public int PlayerNumber { get; set; }
         public DateTime PlayingTime { get; set; }
     }
@@ -29,7 +27,7 @@ namespace Football.Api.Features.Games
 
             var playerNumber = Enumeration.FromValue<PlayerNumber>(request.PlayerNumber);
 
-            var game = new Game(request.PlayingTime, request.Team1Name, request.Team2Name, playerNumber);
+            var game = new Game(request.PlayingTime, playerNumber);
             
             game.EnrollAllPlayers(players);
 
--- Server/src/Football.Core/DomainModels/Game.cs
+++ Server/src/Football.Core/DomainModels/Game.cs
@@ -3,10 +3,8 @@ using Football.Core.ValueObjects;
 
 namespace Football.Core.DomainModels
 {
-    public class Game : AggregateRoot<int>
+    public class Game : Entity
     {
-        public string Team1Name { get; private set; }
-        public string Team2Name { get; private set; }
         public PlayerNumber PlayerNumber { get; private set; }
         public GameResult? GameResult { get; protected set; }
         public DateTime PlayingTime { get; protected set; }
@@ -16,19 +14,11 @@ namespace Football.Core.DomainModels
 
         protected Game() { }
 
-        public Game(DateTime playingTime, string team1Name, string team2Name, PlayerNumber playerNumber)
+        public Game(DateTime playingTime, PlayerNumber playerNumber)
         {
             if (playingTime <= DateTime.Now.AddHours(1))
                 throw new ArgumentException();
 
-            if(string.IsNullOrEmpty(team1Name))
-                throw new ArgumentException();
-
-            if (string.IsNullOrEmpty(team2Name))
-                throw new ArgumentException();
-
-            Team1Name = team1Name;
-            Team2Name = team2Name;
             PlayingTime = playingTime;
             PlayerNumber = playerNumber;
         }
